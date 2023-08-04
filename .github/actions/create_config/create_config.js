8eaa47a7e0ec4cfe282d44bf01d56a002c1f6ac4
Subair Appakkan <subairariyil@gmail.com>
2023-08-04 17:34:01 +0100

    Workflow action Node

---
+++ .github/actions/create_config/create_config.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+function scanFeaturesAndDivide(featureFolder) {
+  // Your script here to scan features and calculate blocks and files per block
+  // Calculate blocks and files_per_block based on your logic
+
+  const blocks = 5;
+  const filesPerBlock = 12;
+
+  return { blocks, filesPerBlock };
+}
+
+async function run() {
+  try {
+    const featureFolder = process.env.INPUT_FEATURE_FOLDER;
+
+    // Call the function to scan features and divide them into blocks
+    const { blocks, filesPerBlock } = scanFeaturesAndDivide(featureFolder);
+
+    // Set the outputs for other steps to use
+    console.log(`::set-output name=blocks::${blocks}`);
+    console.log(`::set-output name=files_per_block::${filesPerBlock}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+run();
