a42c60998e9dabe9881ae3c054bd4fa948f5b002
Subair Appakkan <subairariyil@gmail.com>
2023-08-04 17:59:46 +0100

    Workflow action Node

--- .github/actions/create_config/create_config.js
+++ .github/actions/create_config/create_config.js
@@ -2,27 +2,55 @@ const fs = require('fs');
 const path = require('path');
 
 function scanFeaturesAndDivide(featureFolder) {
-  // Your script here to scan features and calculate blocks and files per block
-  // Calculate blocks and files_per_block based on your logic
+  function findFeatureFiles(folderPath) {
+    const featureFiles = [];
 
-  const blocks = 5;
-  const filesPerBlock = 12;
+    function findFilesRecursively(folderPath) {
+      const files = fs.readdirSync(folderPath);
 
-  return { blocks, filesPerBlock };
+      for (const file of files) {
+        const filePath = path.join(folderPath, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+          findFilesRecursively(filePath);
+        } else if (file.endsWith('.feature')) {
+          featureFiles.push(filePath);
+        }
+      }
+    }
+
+    findFilesRecursively(folderPath);
+    return featureFiles;
+  }
+
+  const allFeatureFiles = findFeatureFiles(featureFolder);
+  const totalFiles = allFeatureFiles.length;
+  const blocks = Math.min(Math.ceil(totalFiles / 5), 5); // Number of blocks should be 5 at most
+  const filesPerBlock = Math.ceil(totalFiles / blocks);
+
+  // Divide files into blocks
+  const blocksArray = new Array(blocks).fill().map((_, index) => {
+    const startIndex = index * filesPerBlock;
+    const endIndex = Math.min((index + 1) * filesPerBlock, totalFiles);
+    return {
+      tags: process.env.INPUT_TAG_NAME,
+      features: allFeatureFiles.slice(startIndex, endIndex),
+    };
+  });
+
+  return blocksArray;
 }
 
 async function run() {
   try {
     const featureFolder = process.env.INPUT_FEATURE_FOLDER;
 
-    // Call the function to scan features and divide them into blocks
-    const { blocks, filesPerBlock } = scanFeaturesAndDivide(featureFolder);
+    // Call the function to scan features, divide them into blocks, and get the result
+    const result = scanFeaturesAndDivide(featureFolder);
 
     // Set the outputs for other steps to use
-    console.log(`blocks=${blocks}`);
-    console.log(`files_per_block=${filesPerBlock}`);
-    console.log(`::set-output name=blocks::${blocks}`);
-    console.log(`::set-output name=files_per_block::${filesPerBlock}`);
+    process.stdout.write(`::set-output name=config_json::${JSON.stringify(result)}\n`);
   } catch (error) {
     console.error(error);
     process.exit(1);
