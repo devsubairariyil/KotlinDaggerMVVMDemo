61d7d9c1caa93a9bfc24cc7307134e77ee56f916
Subair Appakkan <subairariyil@gmail.com>
2023-08-12 14:52:55 +0100

    Variable test

---
+++ .github/actions/test-output/test-output.js
@@ -0,0 +1,21 @@
+// set-outputs.js
+
+const fs = require('fs');
+
+
+async function run() {
+  try {
+    // Calculate the value for 'blocks'
+    const blocks = "some value";
+
+    // Set the output value as an environment variable
+    process.env['BLOCKS_OUTPUT'] = blocks;
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+run();
+
+
--- .github/workflows/test-output.js
+++
@@ -1,9 +0,0 @@
-// set-outputs.js
-
-const fs = require('fs');
-
-// Calculate the value for 'blocks'
-const blocks = "some value";
-
-// Set the output value as an environment variable
-process.env['BLOCKS_OUTPUT'] = blocks;
