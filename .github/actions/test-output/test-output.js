dd3a08e81c4f855d8bb6a80dc6032725bc876040
Subair Appakkan <subairariyil@gmail.com>
2023-08-12 16:02:58 +0100

    Deprecated function

--- .github/actions/test-output/test-output.js
+++ .github/actions/test-output/test-output.js
@@ -12,6 +12,7 @@ async function run() {
     process.env['BLOCKS_OUTPUT'] = blocks;
     process.env['TASK_OUTPUT'] = 'Hhhaaa';
     console.log(`TASK_OUTPUT=HaHa`);
+    process.env.OUTPUT_TEST_DATA = 'This is very special'
     process.stdout.write(`::set-output name=sample::${blocks}\n`);
   } catch (error) {
     console.error(error);
