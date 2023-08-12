8d1039a32a7f6db84754191085048db1cd3b69be
Subair Appakkan <subairariyil@gmail.com>
2023-08-12 16:34:07 +0100

    Shell command in js

--- .github/actions/test-output/test-output.js
+++ .github/actions/test-output/test-output.js
@@ -1,22 +1,14 @@
 // set-outputs.js
 
 const fs = require('fs');
+const core = require('@action/core');
 
 
 async function run() {
   try {
     // Calculate the value for 'blocks'
     const blocks = "some value";
-
-    // Set the output value as an environment variable
-    process.env['BLOCKS_OUTPUT'] = blocks;
-    process.env['TASK_OUTPUT'] = 'Hhhaaa';
-    console.log(`TASK_OUTPUT=HaHa`);
-    process.env.OUTPUT_TEST_DATA = 'This is very special'
-    console.log(`SUM_OUTPUT=${blocks}`);
-    //process.stdout.write(`::set-output name=sample::${blocks}\n`);
-    const shellCommand = `"sample=${blocks}" >> $GITHUB_ENV`;
-    console.log(shellCommand);
+    core.setOutput('test_data', $blocks)
   } catch (error) {
     console.error(error);
     process.exit(1);
