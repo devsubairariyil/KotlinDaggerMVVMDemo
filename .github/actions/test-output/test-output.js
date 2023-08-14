229b394239e2789c31df349bb3f0f8407e8c7c9f
Subair Appakkan <subairariyil@gmail.com>
2023-08-14 09:59:18 +0100

    install node

--- .github/actions/test-output/test-output.js
+++ .github/actions/test-output/test-output.js
@@ -1,12 +1,13 @@
 // set-outputs.js
-const core = require('@actions/core');
 const fs = require('fs');
+const core = require('@actions/core');
 
 
 async function run() {
   try {
     // Calculate the value for 'blocks'
     const blocks = "some value";
+    console.log('test_data:$blocks')
     core.setOutput('test_data', $blocks)
   } catch (error) {
     console.error(error);
