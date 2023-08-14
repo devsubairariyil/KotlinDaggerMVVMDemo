eb688e20f5f7af539643970ae08865fead98a569
Subair Appakkan <subairariyil@gmail.com>
2023-08-14 12:18:04 +0100

    child process - output

--- .github/actions/test-output/test-output.js
+++ .github/actions/test-output/test-output.js
@@ -2,11 +2,4 @@ const { exec } = require('child_process');
 
 const outputValue = 'Your output value'; // Replace this with your actual output value
 
-exec(`echo "MY_OUTPUT_NAME=${outputValue}" >> $GITHUB_ENV`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-  console.log(stdout);
-  console.error(stderr);
-});
+exec(`echo "MY_OUTPUT_NAME=${outputValue}" >> $GITHUB_ENV`);
